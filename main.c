a3d991ac7c584dd487e24f2b6cf9387a8a5529ef
ArturFBAlves <arturfbalves@gmail.com>
2025-08-20 20:18:25 -0300

    adição de altura e peso

--- main.c
+++ main.c
@@ -4,6 +4,8 @@ char disciplina[100];
 char turma[100];
 char nome[100];
 char matricula[100];
+float peso;
+float altura;
 
 int main() {
     printf("Qual a sua disciplina: ");
@@ -18,11 +20,19 @@ int main() {
     printf("Qual a sua matrícula: ");
     scanf(" %s", matricula);
 
+    printf("Qual o seu peso: ");
+    scanf(" %f", &peso);
+
+    printf("Qual a sua altura: ");
+    scanf(" %f", &altura);
+
     printf("\n--- Dados do Aluno ---\n");
     printf("Disciplina: %s\n", disciplina);
     printf("Turma: %s\n", turma);
     printf("Nome: %s\n", nome);
     printf("Matrícula: %s\n", matricula);
+    printf("Altura: %.2f\n", altura);
+    printf("Peso: %.2f", peso);
 
     return 0;
 }
