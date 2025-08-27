4174904bb78313ddefc438479b59dd8a06527f43
ArturFBAlves <arturfbalves@gmail.com>
2025-08-27 20:15:04 -0300

    atividade de condicionais

--- Condicionais/main.c
+++ Condicionais/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+
 int peso, altura;
 char nome[255];
+char classificacao[255];
 double pesoKg, alturaM, imc;
 
 int main() {
@@ -23,10 +25,24 @@ int main() {
     alturaM = altura / 100.0;     
     imc = pesoKg / (alturaM * alturaM);
 
+    if(imc < 18.5) {
+        strcpy(classificacao,"Abaixo do peso" );
+    } else if (18.5 < imc < 24.9) {
+        strcpy(classificacao, "Peso normal");
+    } else if (25 < imc < 29.9) {
+        strcpy(classificacao, "Sobrepeso");
+    } else if (30 < imc < 34.9) {
+        strcpy(classificacao, "Obesidade grau 1");
+    } else if (35 < imc < 39.9) {
+        strcpy(classificacao, "Obesidade grau 2");
+    } else if (40 < imc ) {
+        strcpy(classificacao, "Obesidade grau 3");
+    }
+
     printf("\nNome: %s", nome);
     printf("Altura: %.2f m\n", alturaM);
     printf("Peso: %.2f kg\n", pesoKg);
     printf("IMC: %.2f\n", imc);
-
+    printf("Classificação IMC: %s", classificacao);
     return 0;
 }
